d8d4d80937f3950f3196d7c16e388f364715dfe5
Camille Howard <choward@smith.edu>
2023-02-07 15:25:57 -0500

    test

--- computer.py
+++ computer.py
@@ -1,10 +1,38 @@
 class Computer:
 
-    # What attributes will it need?
-
+    # What attributes will it need? 
+    year_made: int 
+    os = ""
+    price: int
+    desc = ""
+    
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
-
+    def __init__(self, description: str,
+                    processor_type: str,
+                    hard_drive_capacity: int,
+                    memory: int,
+                    operating_system: str,
+                    year_made: int,
+                    price: int):
+        self.description = description
+        self.processor_type = processor_type
+        self.hard_drive_capacity = hard_drive_capacity
+        self.memory = memory
+        self.operating_system = operating_system
+        self.year_made = year_made
+        self.price = price
+    
     # What methods will you need?
+    def refurbish(self, year_made: int, new_os = None) -> None:
+        if self.year_made < 2000:
+            self.price = 0 # too old to sell, donation only
+        elif self.year_made < 2012:
+            self.price = 250 # heavily-discounted price on machines 10+ years old
+        elif self.year_made < 2018:
+            self.price = 550 # discounted price on machines 4-to-10 year old machines
+        else:
+            self.price = 1000 # recent stuff
+        
+        if new_os is not None:
+            self.operating_system = new_os
--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,10 +1,56 @@
+from computer import *
 class ResaleShop:
 
     # What attributes will it need?
+    inventory: list
 
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    def __init__(self):
+        self.inventory = []
 
     # What methods will you need?
+    def buy(self, computer):
+        self.inventory.append(computer)
+        computer.refurbish(computer.year_made, new_os = None)
+    
+    def sell(self, computer) -> object:
+        if computer in self.inventory:
+            self.inventory.remove(computer)
+            return computer
+        else:
+            return None
+    
+    def print_inventory(self):
+        # If the inventory is not empty
+        if self.inventory: 
+            # For each item
+            for computer in self.inventory:
+                # Print its details
+                print("Description:",computer.description,
+                "\nProcessor Type:", computer.processor_type,
+                "\nHard Drive Capacity:", computer.hard_drive_capacity,
+                "\nMemory:", computer.memory,
+                "\nOperating System:", computer.operating_system,
+                "\nYear Made:", computer.year_made,
+                "\nPrice:", computer.price, "\n\n")
+        else:
+            print("No inventory to display.")
+
+def main():
+   #buy computer1 and 2
+    computer1 = Computer("Mac Pro", "3.5 GHc 6-Core Intel Xeon E5", 1024, 64, "old os1", 2013, 1500)
+    computer2 = Computer("desc", "processor", 1, 2, "old os2", 1999, 100)
+    shop = ResaleShop()
+    shop.buy(computer1)
+    shop.buy(computer2)
+    shop.print_inventory()
+    shop.sell(computer1)
+    shop.print_inventory()
+   
+    #print(computer2.price, computer2.description)
+
+   
+ 
+
+main()
